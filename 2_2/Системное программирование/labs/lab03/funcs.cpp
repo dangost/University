13cb0cac61bb50cab35ded4aeceaba8acc4be8b1
Danila Kislitsyn <dangost16@gmail.com>
2021-05-22 13:04:59 +0300

    lab 03 linux

---
+++ 2_2/Системное программирование/labs/lab03/funcs.cpp
@@ -0,0 +1,71 @@
+#include "funcs.h"
+
+using namespace std;
+
+int findMaxIndex(double array[], int len)
+{
+    int maxId = 0;
+    double max = array[maxId];
+
+    for(int i = 1; i < len; i++)
+    {
+        if(max < array[i])
+        {
+            max = array[i];
+            maxId = i;
+        }
+    }
+
+    return maxId;
+}
+
+int findMinIndex(double array[], int len)
+{
+    int minId = 0;
+    double min = array[minId];
+
+    for(int i = 1; i < len; i++)
+    {
+        if(min > array[i])
+        {
+            min = array[i];
+            minId = i;
+        }
+    }
+
+    return minId;
+
+}
+
+double findSumOfFull(double array[], int len)
+{
+    double sum = 0;
+    for(int i = 0; i < len; i++)
+    {
+        sum += array[0];
+    }
+
+    return sum;
+}
+
+double findPowOfFull(double array[], int len)
+{
+    double pow = 1;
+    
+    for(int i = 0; i < len; i++)
+    {
+        pow *= array[i];
+    }
+
+    return pow;
+}
+
+void printArray(double array[], int len)
+{
+    for(int i = 0; i < len; i++)
+    {
+        cout << array[i] << " ";
+    }
+
+    cout << "\n";
+}
