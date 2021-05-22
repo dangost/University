803f404f4c85c1f4f0c5eeb058eda7ad7acadbe4
Danila Kislitsyn <dangost16@gmail.com>
2021-05-22 13:59:57 +0300

    init labs linux

---
+++ 2_2/Системное программирование/labs/lab04/funcs.h
@@ -0,0 +1,19 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+
+#include <iostream>
+
+using namespace std;
+
+int findMaxIndex(double array[], int len);
+
+int findMinIndex(double array[], int len);
+
+double findSumOfFull(double array[], int len);
+
+void printArray(double array[], int len);
+
+double findPowOfFull(double array[], int len);
+
+
+#endif
